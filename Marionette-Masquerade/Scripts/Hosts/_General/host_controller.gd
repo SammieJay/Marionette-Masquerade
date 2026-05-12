bc62287921c6cd9244c2b5155150747a6a13810e
SammieJay <62515526+SammieJay@users.noreply.github.com>
2026-05-12 15:09:42 -0400

    Added dieing

--- Marionette-Masquerade/Scripts/Hosts/_General/host_controller.gd
+++ Marionette-Masquerade/Scripts/Hosts/_General/host_controller.gd
@@ -29,6 +29,7 @@ class_name HostController extends CharacterBody2D
 
 @export_group("Status")
 @export var currentlyPossesable:bool = true
+@export var clearOnDeath:bool = true
 
 @export_group("Movement")
 @export var moveSpeed:float = 20.0
@@ -49,7 +50,7 @@ class_name HostController extends CharacterBody2D
 
 # ----- Health -----
 @onready var alive:bool = true
-var currnentHealth:float
+@onready var currnentHealth:float
 
 # ----- Movement -----
 const MOVE_SPEED_CONSTANT:float = 1200.0
@@ -85,7 +86,7 @@ func _process(_delta):
 	else: enemyController.do_enemy_behavior(_delta)
 
 
-## ===== HOSTMANAGER CALLED FUNCTIONS =====
+## ===== CORE FUNCTIONS CALLED FROM OTHER CLASSES =====
 
 ## Called by HostManager when player switches to a different host [br]
 ## Handles: value changes and effects that occur when switching [b]FROM[/b] this host  [br]
@@ -102,6 +103,22 @@ func possess()->void:
 	currentlyPossessed = true
 	#print(currentlyPossessed)
 	playerController.on_possession()
+
+## Inflict dammage to this host
+func hurt(_dmg:float)->void:
+	currnentHealth -= _dmg
+	if currnentHealth <= 0.0:
+		die()
+
+## Kill host and play death effect
+func die()->void:
+	alive = false
+	effectHandler.play_death_effect()
+	if clearOnDeath: clear()
+
+## Delete & Clear this host from memory
+func clear()->void:
+	queue_free()
 
 
 ## ===== HELPER FUNCTIONS =====
