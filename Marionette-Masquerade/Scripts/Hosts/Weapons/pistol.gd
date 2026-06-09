ac51e0de1c672879eb8a54af98bd6084aebb8eb2
SammieJay <62515526+SammieJay@users.noreply.github.com>
2026-06-09 19:02:25 -0400

    Confusion Working
    base gameplay feels much better
    Confusion que working and updated

--- Marionette-Masquerade/Scripts/Hosts/Weapons/pistol.gd
+++ Marionette-Masquerade/Scripts/Hosts/Weapons/pistol.gd
@@ -6,7 +6,7 @@ func _ready():
 	maxAmmo = 12
 	reloadTime = 2.0
 	damage = 0.5
-	projectileSpeed = 1.5
+	projectileSpeed = 1.75
 	fire_rate = 0.25
 	
 
--- Marionette-Masquerade/Scripts/Hosts/_Core Components/effect_handler.gd
+++ Marionette-Masquerade/Scripts/Hosts/_Core Components/effect_handler.gd
@@ -1,4 +1,4 @@
-## [EffectHandler] – handles effects and animations like activating weapon and switching hosts
+## [HostEffectHandler] – handles effects and animations like activating weapon and switching hosts
 ##
 ## [b]Responsibilities:[/b] [br]
 ##   - Store paths of REQUIRED effects like shooting and host switching [br]
@@ -12,6 +12,10 @@ class_name HostEffectHandler extends AnimationPlayer
 @export var shootEffectName:String = "UNNAMED EFFECT"
 @export var deathEffectName:String = "UNNAMED EFFECT"
 
+@export_category("Stun Effect")
+@export var stunSprite:AnimatedSprite2D
+@export var stunSpriteRotationSpeed:float = 100.0
+
 @export_category("DEBUG")
 @export var doDebugPrints:bool = false
 @export var requireDefaultEffects:bool = false
@@ -20,6 +24,8 @@ class_name HostEffectHandler extends AnimationPlayer
 ## ===== SCRIPT VARIABLES =====
 
 var host:HostController
+
+var stunActive:bool = false
 
 
 ## ===== CORE FUNCTIONS =====
@@ -33,24 +39,37 @@ func _ready():
 		assert(shootEffectName != null, "HostEffectHandler for %s has no SwitchHost effect path" % host.hostTypeName)
 		assert(deathEffectName != null, "HostEffectHandler for %s has no Death effect path" % host.hostTypeName)
 
+		assert(stunSprite, "HostEffectHandler for %s has no stun sprite" % host.hostTypeName)
+
+
+# Mostly for updating stun effect sprite
+func _process(_delta):
+	if stunActive and stunSprite:
+		if !stunSprite.visible: stunSprite.visible = true # show sprite if hidden
+		stunSprite.rotation_degrees += stunSpriteRotationSpeed * _delta
+	elif stunSprite and stunSprite.visible: stunSprite.visible = false # hide sprite if shown and stun)active
 
 ## Start or continue playing effect of given name, if doDebugPrints is enabled and effect not found, will print error to console [br]
 ## [b]Expects:[/b] The given animation name to exist within the animation player [br]
 func update_effect(_name:String):
 	if has_animation(_name):
-		play(_name)
+		queue(_name)
 	elif doDebugPrints: printerr("Animation Not Found In %s: %s" %[host.hostTypeName, _name])
 
 
 ## ===== Mandatory Effect Functions =====
 # Functions that activate effects 
-func play_switch_effect(_hit:bool): 
+func play_switch_effect(_hit:bool):
 	if _hit: update_effect(switchHostEffectName)
 	else: update_effect(switchHostMissEffectName)
 
 func play_death_effect(): update_effect(deathEffectName)
 func play_shoot_effect(): update_effect(shootEffectName)
 func play_stun_effect(): update_effect(stunEffectName)
+
+# Stun effect setter
+func set_stun(_active:bool): stunActive = _active ## Set whether the stun sprite should be active or not
+
 
 
 
